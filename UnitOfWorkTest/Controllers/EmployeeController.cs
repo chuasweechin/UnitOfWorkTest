0ab2d0e9566be8b8d719fd96b19e4d55f78a3903
chuasweechin <sampiochua@gmail.com>
2020-05-16 18:46:50 +0800

    Add test to use transaction

--- UnitOfWorkTest/Controllers/EmployeeController.cs
+++ UnitOfWorkTest/Controllers/EmployeeController.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 
 namespace UnitOfWorkTest.Controllers
@@ -26,15 +29,29 @@ namespace UnitOfWorkTest.Controllers
 		[HttpPost]
 		public void Create()
 		{
-			Employee employee = new Employee
+			try
 			{
-				Id = Guid.NewGuid().ToString(),
-				Name = "Jack",
-				Department = "HR",
-				Email = "Jack@test.com"
-			};
+				string id = Guid.NewGuid().ToString();
+				//string id = "97b526fd-d459-49fd-b7cf-db199caa654e";
 
-			_context.Employees.Add(employee);
+				Employee employee = new Employee
+				{
+				  Id = id,
+					Name = "Jack",
+					Department = "HR",
+					Email = "Jack@test.com"
+				};
+
+				if (_context.Employees.Find(id) != null)
+					throw new DbUpdateException();
+
+				_context.Employees.Add(employee);
+			}
+			catch (DbUpdateException)
+			{
+				Console.WriteLine("Caught");
+				throw;
+			}
 
 			Department department = new Department
 			{
@@ -48,5 +65,58 @@ namespace UnitOfWorkTest.Controllers
 
 			Console.WriteLine("Post");
 		}
+
+		[HttpPost]
+		[Route("v2")]
+		public async Task CreateV2()
+		{
+			var executionStrategy = _context.Database.CreateExecutionStrategy();
+
+			await executionStrategy.ExecuteAsync(async () =>
+			{
+				using (var transaction = _context.Database.BeginTransaction(IsolationLevel.ReadCommitted))
+				{
+					try
+					{
+						Employee employee = new Employee
+						{
+							//Id = Guid.NewGuid().ToString(),
+							Id = new Guid("97b526fd-d459-49fd-b7cf-db199caa654e").ToString(),
+							Name = "Jack v2",
+							Department = "HR v2",
+							Email = "Jack@test.com"
+						};
+
+						_context.Employees.Add(employee);
+						_context.SaveChanges();
+					}
+					catch (DbUpdateException)
+					{
+						Console.WriteLine("Caught");
+						throw;
+					}
+
+					Department department = new Department
+					{
+						Id = Guid.NewGuid().ToString(),
+						Name = "Human Resource v2",
+						Size = 200
+					};
+
+					_context.Departments.Add(department);
+					_context.SaveChanges();
+
+					await transaction.CommitAsync();
+				}
+			});
+
+			Console.WriteLine("Post v2");
+		}
 	}
 }
+
+// ALTER TABLE "Departments" RENAME TO "Department";
+// ALTER TABLE "Employees" RENAME TO "Employee";
+
+// ALTER TABLE "Department" RENAME TO "Departments";
+// ALTER TABLE "Employee" RENAME TO "Employees";
